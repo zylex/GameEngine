a1f79e20cbfcc0bbbcdd1bb72482992794152010
Zylex <zylex@gmail.com>
2015-02-09 02:26:08 +0000

    Forgot to add some files

---
+++ debug_includes/FileHelper.cpp
@@ -0,0 +1,23 @@
+#include "FileHelper.h"
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+namespace zge {
+
+const char* loadFile(const std::string filepath) {
+  std::string contents = "";
+  std::ifstream filestream(filepath, std::ios::in);
+  if (filestream.is_open()) {
+    std::string line = "";
+    while (getline(filestream, line)) {
+      contents += "\n" + line;
+    }
+  } else {
+    std::cerr << "Could not find the file path: " << filepath << std::endl;
+  }
+
+  return (new std::string(contents))->c_str();
+}
+}
---
+++ debug_includes/FileHelper.h
@@ -0,0 +1,9 @@
+#ifndef FILE_HELPER_H
+#define FILE_HELPER_H
+
+#include <string>
+namespace zge {
+const char* loadFile(const std::string);
+}
+
+#endif
